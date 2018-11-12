b141768542da3346492751f191e8f58605b69f95
Pavel Milkevich <pavel.milkevich@gmail.com>
2018-11-12 15:36:38 +0300

    Sandbox list command #32

--- internal/app/commands/sandbox/list.go
+++ internal/app/commands/sandbox/list.go
@@ -2,6 +2,12 @@ package sandbox
 
 import (
 	"github.com/urfave/cli"
+	"path/filepath"
+	"io/ioutil"
+	"fmt"
+	"os"
+	"regexp"
+	"github.com/Masterminds/semver"
 )
 
 var List = cli.Command{
@@ -9,7 +15,47 @@ var List = cli.Command{
 	Aliases: []string{"ls"},
 	Usage:   "List all sandboxes",
 	Action: func(c *cli.Context) error {
-		//TODO: mention the running one if there is one
+		activeDistro := GetActiveDistro()
+		for _, d := range ListDistros() {
+			if activeDistro == d {
+				fmt.Fprintf(os.Stderr, "* %s\n", d)
+			} else {
+				fmt.Fprintf(os.Stderr, "  %s\n", d)
+			}
+		}
 		return nil
 	},
+}
+
+func GetActiveDistro() string {
+	//TODO
+	return ""
+}
+
+func ListDistros() []string {
+	distrosDir := filepath.Join(getHomeDir(), ".enonic", "distributions")
+	files, err := ioutil.ReadDir(distrosDir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not list distros: ", err)
+	}
+	return filterDistros(files, distrosDir)
+}
+
+func filterDistros(vs []os.FileInfo, distrosDir string) []string {
+	vsf := make([]string, 0)
+	for _, v := range vs {
+		if isDistro(v) {
+			vsf = append(vsf, v.Name())
+		} else {
+			if err := os.RemoveAll(filepath.Join(distrosDir, v.Name())); err != nil {
+				fmt.Fprintln(os.Stderr, "Could not remove invalid distro: ", err)
+			}
+		}
+	}
+	return vsf
+}
+
+func isDistro(v os.FileInfo) bool {
+	distroRegexp := regexp.MustCompile(semver.SemVerRegex)
+	return v.IsDir() && distroRegexp.MatchString(v.Name())
 }
--- internal/app/commands/sandbox/sandbox.go
+++ internal/app/commands/sandbox/sandbox.go
@@ -2,14 +2,47 @@ package sandbox
 
 import (
 	"github.com/urfave/cli"
+	"fmt"
+	"os"
+	"github.com/mitchellh/go-homedir"
+	"path/filepath"
 )
 
 func All() []cli.Command {
+	ensureDirStructure()
+
 	return []cli.Command{
 		List,
 		Start,
 		New,
 		Delete,
 		Version,
+	}
+}
+
+func ensureDirStructure() {
+	// Using go-homedir instead of user.Current()
+	// because of https://github.com/golang/go/issues/6376
+	home := getHomeDir()
+	createFolderIfNotExist(home, ".enonic", "distributions")
+	createFolderIfNotExist(home, ".enonic", "sandboxes")
+}
+func getHomeDir() string {
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Could not get user home dir: ", err)
+		os.Exit(1)
+	}
+	return home
+}
+
+func createFolderIfNotExist(paths ...string) {
+	fullPath := filepath.Join(paths...)
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		err = os.MkdirAll(fullPath, 0755)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not create dir: ", err)
+			os.Exit(1)
+		}
 	}
 }
