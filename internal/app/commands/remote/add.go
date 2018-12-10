ffa431b795b19e187b0ff2bbf1195d183f604354
Pavel Milkevich <pavel.milkevich@gmail.com>
2018-12-10 12:30:20 +0300

    Enonic CLI - Remotes - add command #51

--- internal/app/commands/remote/add.go
+++ internal/app/commands/remote/add.go
@@ -2,13 +2,92 @@ package remote
 
 import (
 	"github.com/urfave/cli"
+	"github.com/enonic/xp-cli/internal/app/util"
+	"strings"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"os"
 )
 
 var Add = cli.Command{
-	Name:  "add",
-	Usage: "Add a new remote to list. Format:[name] [user:password]@[scheme]://[host]:[port]",
+	Name: "add",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "url, u",
+			Usage: "Remote url in the following format: [scheme]://[user:password]@[host]:[port]",
+		},
+	},
+	Usage: "Add a new remote to list. Format: [name] [scheme]://[user:password]@[host]:[port]",
 	Action: func(c *cli.Context) error {
 
+		name := ensureNameArg(c)
+		remoteUrl := ensureUrlFlag(c)
+
+		userName := remoteUrl.User.Username()
+		userPass, passSet := remoteUrl.User.Password()
+		if passSet {
+			userPass = generateHash(userPass)
+		}
+
+		data := readRemotesData()
+		data.Remotes[name] = RemoteData{remoteUrl, userName, userPass}
+		writeRemotesData(data)
+
+		fmt.Fprintf(os.Stderr, "Remote '%s' created.", name)
 		return nil
 	},
+}
+
+func generateHash(s string) string {
+	saltedBytes := []byte(s)
+
+	hashedBytes, err := bcrypt.GenerateFromPassword(saltedBytes, bcrypt.DefaultCost)
+	util.Fatal(err, fmt.Sprintf("Could not generate hash from '%s'", s))
+
+	return string(hashedBytes[:])
+}
+
+func ensureUrlFlag(c *cli.Context) *MarshalledUrl {
+	remoteText := c.String("url")
+	var (
+		parsedUrl *MarshalledUrl
+		err       error
+	)
+	util.PromptUntilTrue(remoteText, func(val string, i byte) string {
+		if len(strings.TrimSpace(val)) == 0 {
+			if i == 0 {
+				return "Enter remote URL: "
+			} else {
+				return "Remote URL can not be empty: "
+			}
+		} else {
+			if parsedUrl, err = ParseMarshalledUrl(val); err != nil {
+				return "Incorrect URL. Format: [scheme]://[user:password]@[host]:[port]: "
+			}
+			return ""
+		}
+	})
+	return parsedUrl
+}
+
+func ensureNameArg(c *cli.Context) string {
+	var name string
+	if c.NArg() > 0 {
+		name = c.Args().First()
+	}
+	remotes := readRemotesData()
+	return util.PromptUntilTrue(name, func(val string, i byte) string {
+		if len(strings.TrimSpace(val)) == 0 {
+			if i == 0 {
+				return "Enter the name of the remote: "
+			} else {
+				return "Remote name can not be empty: "
+			}
+		} else {
+			if _, exists := getRemoteByName(val, remotes.Remotes); exists {
+				return fmt.Sprintf("Remote '%s' already exists: ", val)
+			}
+			return ""
+		}
+	})
 }
--- internal/app/commands/remote/remote.go
+++ internal/app/commands/remote/remote.go
@@ -2,13 +2,107 @@ package remote
 
 import (
 	"github.com/urfave/cli"
+	"net/url"
+	"github.com/enonic/xp-cli/internal/app/util"
+	"path/filepath"
+	"bytes"
 )
 
 func All() []cli.Command {
+	ensureDefaultRemoteExists()
+
 	return []cli.Command{
 		Add,
 		Remove,
 		Set,
 		List,
+	}
+}
+
+type MarshalledUrl struct {
+	url.URL
+}
+
+func ParseMarshalledUrl(text string) (*MarshalledUrl, error) {
+	var (
+		err       error
+		parsedUrl *url.URL
+	)
+	if parsedUrl, err = url.ParseRequestURI(text); err != nil {
+		return nil, err
+	}
+	return &MarshalledUrl{*parsedUrl}, err
+}
+
+func (r *MarshalledUrl) UnmarshalText(text []byte) error {
+	var (
+		err       error
+		parsedUrl *url.URL
+	)
+	parsedUrl, err = url.Parse(string(text))
+	r.URL = *parsedUrl
+	return err
+}
+
+func (r *MarshalledUrl) MarshalText() ([]byte, error) {
+	var (
+		err error
+		buf bytes.Buffer
+	)
+	r.User = nil
+	_, err = buf.WriteString(r.String())
+	return buf.Bytes(), err
+}
+
+type RemoteData struct {
+	Url  *MarshalledUrl
+	User string
+	Pass string
+}
+
+type RemotesData struct {
+	Remotes map[string]RemoteData
+}
+
+func readRemotesData() RemotesData {
+	path := filepath.Join(util.GetHomeDir(), ".enonic", ".enonic")
+	file := util.OpenOrCreateDataFile(path, true)
+	defer file.Close()
+
+	var data RemotesData
+	util.DecodeTomlFile(file, &data)
+	if data.Remotes == nil {
+		data.Remotes = make(map[string]RemoteData)
+	}
+	return data
+}
+
+func writeRemotesData(data RemotesData) {
+	path := filepath.Join(util.GetHomeDir(), ".enonic", ".enonic")
+	file := util.OpenOrCreateDataFile(path, false)
+	defer file.Close()
+
+	util.EncodeTomlFile(file, data)
+}
+
+func getRemoteByName(name string, remotes map[string]RemoteData) (*RemoteData, bool) {
+	if remotes == nil {
+		return nil, false
+	}
+	rm, prs := remotes[name]
+	return &rm, prs
+}
+
+func ensureDefaultRemoteExists() {
+	data := readRemotesData()
+	defaultUrl, _ := ParseMarshalledUrl("http://localhost:8080")
+	if remote, exists := getRemoteByName("default", data.Remotes); !exists || remote.Url != defaultUrl {
+		if !exists {
+			remote = &RemoteData{defaultUrl, "", ""}
+		} else {
+			remote.Url = defaultUrl
+		}
+		data.Remotes["default"] = *remote
+		writeRemotesData(data)
 	}
 }
