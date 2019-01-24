7f083c8204a4569d4f8188382268c8f9c48db799
Pavel Milkevich <pavel.milkevich@gmail.com>
2019-01-24 16:00:07 +0300

    Group commands in two sections (ala Docker) to make things clearer #69
    - also made custom templates for commands and sub-commands

--- internal/app/cli.go
+++ internal/app/cli.go
@@ -10,12 +10,63 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "Enonic CLI"
+	app.Version = "0.1.12"
 	app.Usage = "Manage XP instances, home folders and projects"
 	app.EnableBashCompletion = true
 	app.Commands = commands.All()
+
+	app.CustomAppHelpTemplate = appHelp
+	cli.CommandHelpTemplate = commandHelp
+	cli.SubcommandHelpTemplate = subCommandHelp
 
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+var subCommandHelp = `
+{{if .Description}}{{.Description}}{{else}}{{.Usage}}{{end}}
+
+USAGE:
+   {{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}} command{{if .VisibleFlags}} [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}
+
+COMMANDS:{{range .VisibleCategories}}{{if .Name}}
+   {{.Name}}:{{end}}{{range .VisibleCommands}}
+     {{join .Names ", "}}{{"\t"}}{{.Usage}}{{end}}
+{{end}}{{if .VisibleFlags}}
+OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+`
+
+var commandHelp = `
+{{if .Description}}{{.Description}}{{else}}{{.Usage}}{{end}}
+
+USAGE:
+   {{if .UsageText}}{{.UsageText}}{{else}}{{.HelpName}}{{if .VisibleFlags}} [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{end}}{{if .Category}}
+
+CATEGORY:
+   {{.Category}}{{end}}{{if .VisibleFlags}}
+
+OPTIONS:
+   {{range .VisibleFlags}}{{.}}
+   {{end}}{{end}}
+`
+
+var appHelp = `
+{{.Name}} v.{{.Version}}
+{{.Usage}}
+
+USAGE:
+   {{.HelpName}} {{if .VisibleFlags}}[global options]{{end}}{{if .Commands}} command [command options]{{end}} {{if .ArgsUsage}}{{.ArgsUsage}}{{else}}[arguments...]{{end}}{{if .VisibleCommands}}
+
+COMMANDS:{{range .VisibleCategories}}{{if .Name}}
+   {{ "\n" }}{{.Name}}:{{end}}{{range .VisibleCommands}}
+     {{join .Names ", "}}{{"\t"}}{{.Usage}}{{end}}{{end}}{{end}}{{if .VisibleFlags}}
+
+GLOBAL OPTIONS:
+{{range .VisibleFlags}}   {{.}}{{ "\n" }}{{end}}{{end}}
+
+{{if .Copyright }}{{.Copyright}}{{end}}
+`
--- internal/app/commands/commands.go
+++ internal/app/commands/commands.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"github.com/urfave/cli"
+	"github.com/enonic/xp-cli/internal/app/commands/sandbox"
+	"github.com/enonic/xp-cli/internal/app/commands/project"
 	"github.com/enonic/xp-cli/internal/app/commands/snapshot"
 	"github.com/enonic/xp-cli/internal/app/commands/dump"
 	"github.com/enonic/xp-cli/internal/app/commands/export"
@@ -9,8 +11,6 @@ import (
 	"github.com/enonic/xp-cli/internal/app/commands/repo"
 	"github.com/enonic/xp-cli/internal/app/commands/cms"
 	"github.com/enonic/xp-cli/internal/app/commands/cluster"
-	"github.com/enonic/xp-cli/internal/app/commands/sandbox"
-	"github.com/enonic/xp-cli/internal/app/commands/project"
 )
 
 func All() []cli.Command {
@@ -18,47 +18,58 @@ func All() []cli.Command {
 		{
 			Name:        "snapshot",
 			Usage:       "Snapshot commands",
+			HelpName:    "Snapshot",
 			Subcommands: snapshot.All(),
 		},
 		{
 			Name:        "dump",
 			Usage:       "Dump commands",
+			HelpName:    "Dump",
 			Subcommands: dump.All(),
 		},
 		{
 			Name:        "export",
 			Usage:       "Export commands",
+			HelpName:    "Export",
 			Subcommands: export.All(),
 		},
 		{
 			Name:        "app",
 			Usage:       "Application commands",
+			HelpName:    "Application",
 			Subcommands: app.All(),
 		},
 		{
 			Name:        "repo",
 			Usage:       "Repository commands",
+			HelpName:    "Repo",
 			Subcommands: repo.All(),
 		},
 		{
 			Name:        "cms",
 			Usage:       "CMS commands",
+			HelpName:    "CMS",
 			Subcommands: cms.All(),
 		},
 		{
 			Name:        "cluster",
 			Usage:       "Cluster commands",
+			HelpName:    "Cluster",
 			Subcommands: cluster.All(),
 		},
 		{
 			Name:        "sandbox",
 			Usage:       "Sandbox commands",
 			Subcommands: sandbox.All(),
+			HelpName:    "Sandbox",
+			Category:    "PROJECT COMMANDS",
 		},
 		{
 			Name:        "project",
 			Usage:       "Project commands",
 			Subcommands: project.All(),
+			HelpName:    "Project",
+			Category:    "PROJECT COMMANDS",
 		},
 		/*
 				{
