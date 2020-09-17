d8bf1664968c02409c5f46256f6eec179b670f84
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2020-09-17 14:54:51 +0200

    Just add client id and secret to source #245

--- internal/app/commands/cloud/auth/constants.go
+++ internal/app/commands/cloud/auth/constants.go
@@ -1,8 +1,8 @@
 package auth
 
 var (
-	clientID     = "<SET_DURING_BUILD>"
-	clientSecret = "<SET_DURING_BUILD>"
+    clientID     = "WMq5N474JWbzIHY5RanLa5z4mConLz6C"
+    clientSecret = "D1EIXMyJqCjmOB3bbaVGQ2hT5Yr3S_pxjzn1uaLQflPUnm6bA3IHNm5Egj4diMBb"
 	scope        = "openid profile email offline_access"
 	authURL      = "https://enonic.eu.auth0.com"
 )
