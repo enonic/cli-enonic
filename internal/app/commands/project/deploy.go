7c154b27278a79a30d9e870e06bbdf9979c4ad7b
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-04-15 15:49:18 +0300

    Give feedback when project deploy is executed with dev parameter #274

--- internal/app/commands/project/deploy.go
+++ internal/app/commands/project/deploy.go
@@ -5,7 +5,9 @@ import (
 	"github.com/enonic/cli-enonic/internal/app/commands/common"
 	"github.com/enonic/cli-enonic/internal/app/commands/sandbox"
 	"github.com/enonic/cli-enonic/internal/app/util"
+	"github.com/fatih/color"
 	"github.com/urfave/cli"
+	"os"
 )
 
 var Deploy = cli.Command{
@@ -28,11 +30,13 @@ var Deploy = cli.Command{
 		if projectData := ensureProjectDataExists(c, ".", "A sandbox is required to deploy the project, do you want to create one?"); projectData != nil {
 			runGradleTask(projectData, fmt.Sprintf("Deploying to sandbox '%s'...", projectData.Sandbox), "deploy")
 
+			fmt.Fprintln(os.Stderr, "")
+
 			rData := common.ReadRuntimeData()
 			processRunning := common.VerifyRuntimeData(&rData)
 
 			if !processRunning {
-				if util.PromptBool(fmt.Sprintf("\nDo you want to start sandbox '%s'?", projectData.Sandbox), true) {
+				if util.PromptBool(fmt.Sprintf("Do you want to start sandbox '%s'?", projectData.Sandbox), true) {
 					sandbox.StartSandbox(sandbox.ReadSandboxData(projectData.Sandbox), false, devMode, debug)
 				}
 			} else if rData.Running != projectData.Sandbox {
@@ -41,6 +45,9 @@ var Deploy = cli.Command{
 					sandbox.StopSandbox(rData)
 					sandbox.StartSandbox(sandbox.ReadSandboxData(projectData.Sandbox), false, devMode, debug)
 				}
+			} else {
+				// Desired sandbox is already running, just give a heads up about  --dev and --debug params
+				color.New(color.FgCyan).Fprintf(os.Stderr, "Sandbox '%s' is already running. --dev and --debug parameters ignored\n\n", projectData.Sandbox)
 			}
 		}
 
