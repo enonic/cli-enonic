ab550f2897c3b473d9064eb20f50e1d472f3e4d3
Pavel Milkevich <pavel.milkevich@gmail.com>
2021-11-03 15:54:59 +0300

    app start / stop is documented but not existing #321

--- internal/app/commands/app/app.go
+++ internal/app/commands/app/app.go
@@ -1,11 +1,37 @@
 package app
 
 import (
+	"cli-enonic/internal/app/commands/common"
+	"cli-enonic/internal/app/util"
+	"fmt"
+	"github.com/pkg/errors"
 	"github.com/urfave/cli"
+	"os"
+	"strings"
 )
 
 func All() []cli.Command {
 	return []cli.Command{
 		Install,
+		Start,
+		Stop,
 	}
+}
+
+func ensureAppKeyArg(c *cli.Context) string {
+	force := common.IsForceMode(c)
+	keyValidator := func(val interface{}) error {
+		str := val.(string)
+		if len(strings.TrimSpace(str)) == 0 {
+			if force {
+				fmt.Fprintln(os.Stderr, "Application key can not be empty in non-interactive mode.")
+				os.Exit(1)
+			}
+			return errors.New("Application key can not be empty")
+		} else {
+			return nil
+		}
+	}
+
+	return util.PromptString("Enter application key", c.Args().First(), "", keyValidator)
 }
---
+++ internal/app/commands/app/stop.go
@@ -0,0 +1,56 @@
+package app
+
+import (
+	"bytes"
+	"cli-enonic/internal/app/commands/common"
+	"encoding/json"
+	"fmt"
+	"github.com/urfave/cli"
+	"net/http"
+	"os"
+)
+
+var Stop = cli.Command{
+	Name:      "stop",
+	Usage:     "Stop an application",
+	ArgsUsage: "<app key>",
+	Flags:     append([]cli.Flag{}, common.AUTH_FLAG, common.FORCE_FLAG),
+	Action: func(c *cli.Context) error {
+
+		key := ensureAppKeyArg(c)
+
+		stopApp(c, key)
+
+		return nil
+	},
+}
+
+func stopApp(c *cli.Context, name string) {
+	req := createStopRequest(c, name)
+
+	res := common.SendRequest(req, fmt.Sprintf("Requesting stop \"%s\"", name))
+
+	var status string
+	if res.StatusCode >= 200 && res.StatusCode < 300 {
+		status = "Done"
+	} else {
+		status = "Error"
+	}
+
+	fmt.Fprintln(os.Stdout, status)
+}
+
+func createStopRequest(c *cli.Context, key string) *http.Request {
+	body := new(bytes.Buffer)
+	params := map[string]string{
+		"key": key,
+	}
+	json.NewEncoder(body).Encode(params)
+	req := common.CreateRequest(c, "POST", "app/stop", body)
+
+	return req
+}
+
+type StopResult struct {
+	//TODO: add body when xp#9189 is done
+}
