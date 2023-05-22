10114e64e42dd38090ac18baa52c11863d496e51
Pavel Milkevich <pmi@enonic.com>
2023-05-22 15:55:41 +0200

    Add uninstall command #438

---
+++ internal/app/commands/system/uninstall.go
@@ -0,0 +1,29 @@
+package system
+
+import (
+	"cli-enonic/internal/app/commands/common"
+	"cli-enonic/internal/app/util"
+	"cli-enonic/internal/app/util/system"
+	"github.com/urfave/cli"
+	"strings"
+)
+
+var Uninstall = cli.Command{
+	Name:  "uninstall",
+	Usage: "Uninstall Enonic CLI",
+	Flags: []cli.Flag{common.AUTH_FLAG, common.FORCE_FLAG},
+	Action: func(c *cli.Context) error {
+
+		if answer := common.IsForceMode(c) ||
+			util.PromptBool("Do you want to remove Enonic CLI from your system", false); answer {
+
+			isNPM := common.IsInstalledViaNPM()
+			uninstallCommand := common.GetOSUninstallCommand(isNPM)
+			uninstallArgs := strings.Split(uninstallCommand, " ")
+
+			system.Run(uninstallArgs[0], uninstallArgs[1:])
+		}
+
+		return nil
+	},
+}
