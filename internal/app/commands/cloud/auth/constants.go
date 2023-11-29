5b21d9032c1b7352acb19d849299d8ae4a4775ee
Pavel Milkevich <pmi@enonic.com>
2023-11-29 10:24:25 +0100

    Make authURL and clientID overridable via env variables #477

--- internal/app/commands/cloud/auth/constants.go
+++ internal/app/commands/cloud/auth/constants.go
@@ -1,7 +1,31 @@
 package auth
 
-var (
-    clientID     = "WMq5N474JWbzIHY5RanLa5z4mConLz6C"
-	scope        = "openid profile email offline_access"
-	authURL      = "https://enonic.eu.auth0.com"
+import (
+	"os"
 )
+
+const CLI_CLOUD_AUTH_CLIENT_VAR = "ENONIC_CLI_CLOUD_AUTH_CLIENT"
+const CLI_CLOUD_AUTH_CLIENT_DEFAULT = "WMq5N474JWbzIHY5RanLa5z4mConLz6C"
+
+const CLI_CLOUD_AUTH_URL_VAR = "ENONIC_CLI_CLOUD_AUTH_URL"
+const CLI_CLOUD_AUTH_URL_DEFAULT = "https://auth.enonic.com"
+
+var (
+	clientID = getAuthClient()
+	scope    = "openid profile email offline_access"
+	authURL  = getAuthUrl()
+)
+
+func getAuthUrl() string {
+	if userUrl := os.Getenv(CLI_CLOUD_AUTH_URL_VAR); userUrl != "" {
+		return userUrl
+	}
+	return CLI_CLOUD_AUTH_URL_DEFAULT
+}
+
+func getAuthClient() string {
+	if userClient := os.Getenv(CLI_CLOUD_AUTH_CLIENT_VAR); userClient != "" {
+		return userClient
+	}
+	return CLI_CLOUD_AUTH_CLIENT_DEFAULT
+}
