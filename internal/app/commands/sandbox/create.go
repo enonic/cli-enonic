411437620eae890ba112a33ca64c14fbff1109b9
Pavel Milkevich <pmi@enonic.com>
2024-03-04 16:58:00 +0100

    Start sandbox in dev mode by default #517

--- internal/app/commands/sandbox/create.go
+++ internal/app/commands/sandbox/create.go
@@ -66,8 +66,8 @@ var Create = cli.Command{
 			Usage: "List all distro versions.",
 		},
 		cli.BoolFlag{
-			Name:  "dev",
-			Usage: "Use development mode when starting sandbox",
+			Name:  "prod",
+			Usage: "Run Enonic XP distribution in non-development mode",
 		},
 		common.FORCE_FLAG,
 	},
