51684013f54242dc0f96df480ffc5d02ea9aa0f6
Sergey Rymsha <rymsha@users.noreply.github.com>
2026-05-09 10:04:06 +0200

    Update common_message_test.go

--- internal/app/commands/common/common_message_test.go
+++ internal/app/commands/common/common_message_test.go
@@ -3,7 +3,7 @@ package common
 import "testing"
 
 func TestRestartAllRunningInstancesMessage(t *testing.T) {
-	expected := "Please restart all running instances of XP."
+	expected := "Please restart XP instance/cluster."
 	if RESTART_ALL_RUNNING_INSTANCES_MSG != expected {
 		t.Fatalf("expected %q, got %q", expected, RESTART_ALL_RUNNING_INSTANCES_MSG)
 	}
